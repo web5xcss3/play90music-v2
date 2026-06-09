3c6ea5dd9cfed0558db5fd6e15aec63cf92c5b28
W53 <web5xcss3@gmail.com>
2026-06-08 22:48:24 -0300

    Update helpers.js

--- assets/js/modules/helpers.js
+++ assets/js/modules/helpers.js
@@ -58,12 +58,17 @@
 
     window.CDN_BASE =
         'https://cdn.jsdelivr.net/gh/web5xcss3/play90music-v2@main';
-    
+
+    window.APP_VERSION =
+        window.APP_VERSION || '1.0.3';
+
     window.resolveAsset = function(path) {
+
+        if (!path) return '';
 
         if (path.startsWith('http')) return path;
 
-        return `${window.CDN_BASE}/${path}`;
+        return `${window.CDN_BASE}/${path}?v=${window.APP_VERSION}`;
     };
 
     window.loadScriptOnce = function(id, src, callback) {
@@ -76,14 +81,19 @@
         const script = document.createElement('script');
 
         script.id = id;
-        script.src = resolveAsset(src);
+        script.src = window.resolveAsset(src);
+        script.async = false;
+
+        console.log('Carregando script dinâmico:', script.src);
 
         script.onload = function() {
+            console.log('Script dinâmico carregado:', script.src);
+
             if (typeof callback === 'function') callback();
         };
 
         script.onerror = function() {
-            console.error('Erro ao carregar script:', src);
+            console.error('Erro ao carregar script:', script.src);
         };
 
         document.body.appendChild(script);
